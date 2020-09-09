dc995ee84cc0a5df47248a79d482ed9f90a0e1ac
zxyle <zxyful@gmail.com>
2020-09-09 08:37:45 +0800

    Back up files before modifying

--- .scripts/modify-debian-mirror.sh
+++ .scripts/modify-debian-mirror.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+cp /etc/apt/sources.list /etc/apt/sources.list.bak
 sed -i s@/deb.debian.org/@/mirrors.aliyun.com/@g /etc/apt/sources.list
 sed -i s@/security.debian.org/@/mirrors.aliyun.com/@g /etc/apt/sources.list
--- .scripts/modify-ubuntu-mirror.sh
+++ .scripts/modify-ubuntu-mirror.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+cp /etc/apt/sources.list /etc/apt/sources.list.bak
 sed -i s@/archive.ubuntu.com/@/mirrors.aliyun.com/@g /etc/apt/sources.list
 sed -i s@/security.ubuntu.com/@/mirrors.aliyun.com/@g /etc/apt/sources.list
